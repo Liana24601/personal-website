48dcfdd41c12e337e5ae3afcb408dbb2d08b48ca
Liana H. <lianadothallik@gmail.com>
2025-06-07 11:00:23 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-  function likeReview(button) {
-    const countSpan = button.nextElementSibling;
-    let count = parseInt(countSpan.innerText);
-    count++;
-    countSpan.innerText = count;
-  }
-</script>
