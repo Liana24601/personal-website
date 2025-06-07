eeb19fbc2ebdbcd3dd5e786f74aa0a1c21eda0ff
Liana H. <lianadothallik@gmail.com>
2025-06-07 10:55:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+<script>
+  function likeReview(button) {
+    const countSpan = button.nextElementSibling;
+    let count = parseInt(countSpan.innerText);
+    count++;
+    countSpan.innerText = count;
+  }
+</script>
